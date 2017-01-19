86af4f967e1fcfbe048e4615b830683c94eff9b5
Manuelbn <Manuel.neuberger@live.de>
2017-01-19 13:39:52 +0100

    Menu hinzugefugt (noch nicht angepasst)

---
+++ Assets/Menu/Scripte/Loadscripte.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Loadscripte : MonoBehaviour {
+
+    private Vector3 pos;
+    private bool loaded;
+    public static Loadscripte load;
+	// Use this for initialization
+	void Awake () {
+        DontDestroyOnLoad(gameObject);
+        if (load == null)
+        {
+            load = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+	}
+    void Start()
+    {
+        loaded = false;
+    }
+
+    public Vector3 getPos()
+    {
+        return pos;
+    }
+    public void setPos(Vector3 p)
+    {
+        pos = p;
+    }
+    public bool getLoaded()
+    {
+        return loaded;
+    }
+    public void setLoaded(bool l)
+    {
+        loaded = l;
+    }
+}
--- Assets/Menu/Scripte/Pausenscripte.cs
+++ Assets/Menu/Scripte/Pausenscripte.cs
@@ -1,27 +1,53 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using UnityEngine.UI;
 
-public class Pausenscripte : MonoBehaviour {
+
+public class Pausenscripte : MonoBehaviour
+{
+    // Charakter as well as the CharakterCamera
     public GameObject Charakter;
     public GameObject CharakterCamera;
+    // The Camera that looks at the Menu
     public GameObject Camera;
+    // The canvas of the Paused game
     public Canvas PauseCanvas;
-    public float charakterHight;
+    // The Canvas that opens if you press end game
     public Canvas reallyEndC;
+    // Opens if the game was saved
+    public Canvas savedC;
+
+    // The Scripte handels the pausing and continuing of the scene as well as saving and loading
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         PauseCanvas.enabled = false;
-        Cursor.lockState= CursorLockMode.Locked;
+        savedC.enabled = false;
+        Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
         reallyEndC.enabled = false;
+        Camera.SetActive(false);
+        Charakter.SetActive(true);
+        Time.timeScale = 1;
+        setInteractiable(true,PauseCanvas.GetComponents<Button>());
+
+        // Laden teil
+        if (Loadscripte.load.getLoaded())
+        {
+            Charakter.transform.position = Loadscripte.load.getPos();
+        }
+
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            
             PauseCanvas.enabled = !PauseCanvas.enabled;
             if (PauseCanvas.enabled)
             {
@@ -31,23 +57,24 @@ public class Pausenscripte : MonoBehaviour {
                 Camera.transform.position = CharakterCamera.transform.position;
                 Camera.transform.rotation = CharakterCamera.transform.rotation;
                 Charakter.SetActive(false);
-                
+
                 Camera.SetActive(true);
             }
             else
             {
+                // closes the Menu and starts the game again
                 Time.timeScale = 1;
                 Cursor.lockState = CursorLockMode.Locked;
                 Charakter.SetActive(true);
                 Camera.SetActive(false);
                 Cursor.visible = false;
+                reallyEndC.enabled = false;
             }
-            
-
         }
-	}
 
-    public void  continueGame()
+    }
+
+    public void continueGame()
     {
         PauseCanvas.enabled = !PauseCanvas.enabled;
         Time.timeScale = 1;
@@ -55,13 +82,94 @@ public class Pausenscripte : MonoBehaviour {
         Charakter.SetActive(true);
         Camera.SetActive(false);
     }
+    // Is Called if you end the game to check if you really want to end
     public void reallyEnd()
     {
         reallyEndC.enabled = true;
+        setInteractiable(false, PauseCanvas.GetComponentsInChildren<Button>());
+
     }
+    // Is Called if you don´t really want to end
     public void notEnd()
     {
+        setInteractiable(true, PauseCanvas.GetComponentsInChildren<Button>());
         reallyEndC.enabled = false;
     }
+    // Goes back to the Menu after pressing yes
+    public void backToMainMenu()
+    {
+        Loadscripte.load.setLoaded(false);
+        SceneManager.LoadScene("Start Menu");
+    }
 
+    private void setInteractiable(bool b, Button[] Button)
+    {
+        for(int i =0; i<Button.Length;i++)
+        {
+            Button[i].interactable = b;
+        }
+    }
+
+    // Save and Load part
+
+    // Saves the players position and rotation into a file called playerProgress.dat
+    private void Save()
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/playerProgress.dat");
+        playerProgress plPro = new playerProgress();
+        plPro.pos = vecToArray(Charakter.transform.position);
+        // plPro.rot = vecToArray(Charakter.transform.rotation.eulerAngles);
+        bf.Serialize(file, plPro);
+        file.Close();
+
+    }
+    
+
+    // Because only primary datatypes can be serialysed, dies Methodes convert Vec to an array an back
+    private float[] vecToArray(Vector3 vec)
+    {
+        float[] f = new float[3];
+        f[0] = vec.x;
+        f[1] = vec.y;
+        f[2] = vec.z;
+        return f;
+    }
+   
+    // Saves the game and opens the Field to press ok
+    public void openSave()
+    {
+        Save();
+        savedC.enabled = true;
+        setInteractiable(false, PauseCanvas.GetComponentsInChildren<Button>());
+    }
+
+    public void continueAfterSave()
+    {
+        savedC.enabled = false;
+        setInteractiable(true, PauseCanvas.GetComponentsInChildren<Button>());
+    }
+
+    // Because only primary datatypes can be serialysed, dies Methodes convert Quaternion to an array an back
+    /*
+        public float[] quadToArray(Quaternion Quat)
+        {
+            float[] f = new float[4];
+            f[0] = Quat.x;
+            f[1] = Quat.y;
+            f[2] = Quat.z;
+            f[3] = Quat.w;
+            return f;
+        }
+        public Quaternion arrayToQuat(float[] f)
+        {
+            return new Quaternion(f[0], f[1], f[2],f[3]);
+        }*/
 }
+[System.Serializable]
+class playerProgress
+{
+    public float[] pos = new float[3];
+    // public float[] rot = new float[3];
+}
+
