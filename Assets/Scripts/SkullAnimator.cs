41ee43eca12874ffca0383dbc1d8cab5451838dd
Jean-Luc Etgen <jean-luc@etgen.de>
2017-01-20 13:57:58 +0100

    added animation for skulls

---
+++ Assets/Scripts/SkullAnimator.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkullAnimator : MonoBehaviour {
+
+    private bool closed;
+    private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+
+        Close();
+    }
+
+    public void Open()
+    {
+        closed = false;
+
+        animator.SetBool("closed", closed);
+    }
+
+    public void Close()
+    {
+        closed = true;
+
+        animator.SetBool("closed", closed);
+    }
+
+    public bool isClosed()
+    {
+        return closed;
+    }
+}
