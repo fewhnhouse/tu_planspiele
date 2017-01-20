60408e0c82e73684e2d88c039cab5f890d736d40
borchera <alexb95@live.de>
2017-01-20 19:57:49 +0100

    disabled characterController while player OnBoat

--- Assets/Scripts/BoatMovement.cs
+++ Assets/Scripts/BoatMovement.cs
@@ -8,6 +8,7 @@ public class BoatMovement : MonoBehaviour, Activatable
     public CharacterController playerCC;
     public GameObject playerPosition;
     public GameObject figureFromDirectionGame;
+    public bool playerOnBoat;
 
     private Animator myAnimator;
 
@@ -21,9 +22,21 @@ public class BoatMovement : MonoBehaviour, Activatable
 
     public void Activate()
     {
+
         if (figureFromDirectionGame.GetComponent<FigureMovementBehaviourScript>().won)
         {
-            Debug.Log("activate");
+            playerCC.enabled = false;
+            playerOnBoat = true;
+            playerPosition.transform.position = getBoatPosition();
         }
+        else
+        {
+            Debug.Log("Win first");
+        }
+    }
+
+    private Vector3 getBoatPosition()
+    {
+        return new Vector3(transform.position.x + 1.0f, transform.position.y + 2.5f, transform.position.z);
     }
 }
