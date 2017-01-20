86db15e6b80da42da0e04b1512c6d7a644eb431d
Jean-Luc Etgen <jean-luc@etgen.de>
2017-01-20 13:16:14 +0100

    added trapdoor animation + prefab

---
+++ Assets/Scripts/TrapDoorAnimation.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapDoorAnimation: MonoBehaviour {
+    private bool closed;
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void Open()
+    {
+        closed = false;
+
+        animator.SetBool("closed", closed);
+    }
+
+    public void Close()
+    {
+        closed = true;
+
+        animator.SetBool("closed", closed);
+    }
+
+    public bool isClosed()
+    {
+        return closed;
+    }
+}
