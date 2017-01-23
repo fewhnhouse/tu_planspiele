7074d865d480ec15ed5c0e91b938eddcbf0cf0aa
Jean-Luc Etgen <jean-luc@etgen.de>
2017-01-23 20:21:00 +0100

    Added script to load a new scene when entering a trigger

---
+++ Assets/Scripts/LoadNewSceneTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(Collider))]
+public class LoadNewSceneTrigger : MonoBehaviour {
+    public string NewScene;
+
+    void OnTriggerEnter(Collider collider)
+    {
+        SceneManager.LoadScene(NewScene);
+    }
+}
