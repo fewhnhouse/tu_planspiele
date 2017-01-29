58c78d795261699cd7f132bf135e406028e59973
Jean-Luc Etgen <jean-luc@etgen.de>
2017-01-29 02:42:59 +0100

    load credits scene after opening chest in final level

---
+++ Assets/FinalGame/Scripts/LoadSceneTimer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneTimer : MonoBehaviour {
+    public string NewScene;
+    public float Delay;
+
+    private bool TimerStarted;
+    // Use this for initialization
+    public void StartTimer()
+    {
+        if (!TimerStarted)
+        {
+            TimerStarted = true;
+            StartCoroutine(timer(Delay));
+        }
+    }
+
+    private IEnumerator timer(float Delay)
+    {
+        yield return new WaitForSeconds(Delay);
+
+        SceneManager.LoadScene(NewScene);
+    }
+}
