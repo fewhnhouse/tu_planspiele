8aacab58519acff3a82e8fbcbbed4aed230dd2ee
Manuelbn <Manuel.neuberger@live.de>
2017-01-31 15:16:09 +0100

    Creadits

---
+++ Assets/Menu/Scripte/CreaditsMove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreaditsMove : MonoBehaviour {
+
+    private float cooldown = 2.0f;
+    public GameObject gameObject;
+    public GameObject target;
+    private float destiany;
+    public int speed = 3;
+    // Use this for initialization
+    private void Start()
+    {
+        destiany = target.transform.position.y;
+    }
+
+    public void Update()
+    {
+        if (cooldown >= 0)
+        {
+            cooldown -= Time.deltaTime;
+        }
+        else
+        {
+            if (gameObject.transform.position.y <= destiany)
+            {
+                Vector3 v = gameObject.transform.position;
+                gameObject.transform.position = new Vector3(v.x,v.y + Time.deltaTime*speed, v.z);
+            }
+        }
+    }
+}
